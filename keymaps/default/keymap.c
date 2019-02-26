c9457bb595977279fbf546521d9a1fd6e88ab08e
Isaac Blackburn <isaac@tblackburn.me>
2019-02-25 23:49:46 -0500

    removed junk files

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -15,26 +15,170 @@
  */
 #include QMK_KEYBOARD_H
 
+#define _PICK 0
+#define _PREM 1
+#define _PS 2
+#define _KICAD 3
+#define _MEDIA 4
+#define _NONE 16
+
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT( /* Base */
-    KC_A,  KC_B,  KC_C, KC_D, \
-    KC_E,  KC_F,  KC_G, KC_H, \
-    KC_I,  KC_J,  KC_K, KC_L \
+/*.-----------.-----------.-----------.-----------.
+  |           |           |/         \|/ Layer   \|
+  |           |           |\         /|\ Select  /|
+  |-----------+-----------+-----------+-----------|
+  | Media     |           |           |           |
+  |           |           |           |           |
+  |-----------+-----------+-----------+-----------|
+  | Premiere  | Photoshop | KiCad     | None      |
+  |           |           |           |           |
+  `-----------'-----------'-----------'-----------'*/
+  [_PICK] = LAYOUT( /* Layer Select */
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+    DF(_MEDIA), KC_TRNS, KC_TRNS, KC_TRNS, \
+    DF(_PREM), DF(_PS), DF(_KICAD), DF(_NONE) \
+  ),
+/*.-----------.-----------.-----------.-----------.
+  | Timeline  | Timeline  |/ Save    \|/ Layer   \|
+  | Select    | Cut       |\ Ctrl+S  /|\ Select  /|
+  |-----------+-----------+-----------+-----------|
+  | Insert    | Overwrite | Delete    | Enter     |
+  |           |           |           |           |
+  |-----------+-----------+-----------+-----------|
+  | Shift     | Control   | Alt       | Space      |
+  |           |           |           |           |
+  `-----------'-----------'-----------'-----------'*/
+  [_PREM] = LAYOUT( /* Premiere */
+    KC_V,  KC_C,  LCTL(KC_S), DF(_PICK), \
+    KC_COMMA,  KC_DOT,  KC_DELETE, KC_ENTER, \
+    KC_LSFT,  KC_LCTL,  KC_LALT, KC_SPACE \
+  ),
+/*.-----------.-----------.-----------.-----------.
+  | Selection | Wire      |/         \|/ Layer   \|
+  | Tool      | Tool      |\         /|\ Select  /|
+  |-----------+-----------+-----------+-----------|
+  | Shift     | New       | Move      | Delete    |
+  |           | Track     |           |           |
+  |-----------+-----------+-----------+-----------|
+  | Control   |           |           |           |
+  |           |           |           |           |
+  `-----------'-----------'-----------'-----------'*/
+  [_KICAD] = LAYOUT( /* KiCad */
+    KC_ESC,  KC_W,  KC_C, DF(_PICK), \
+    KC_LSFT,  KC_X,  KC_M, KC_DEL, \
+    KC_LCTL,  KC_I,  KC_J, KC_K \
+  ),
+/*.-----------.-----------.-----------.-----------.
+  | S         | R         |/ C       \|/ Layer   \|
+  |           |           |\         /|\ Select  /|
+  |-----------+-----------+-----------+-----------|
+  | Shift     | E         | F         | G         |
+  |           |           |           |           |
+  |-----------+-----------+-----------+-----------|
+  | Control   | Alt       | Space     | K         |
+  |           |           |           |           |
+  `-----------'-----------'-----------'-----------'*/
+  [_PS] = LAYOUT( /* Photoshop */
+    KC_S,  KC_R,  KC_C, DF(_PICK), \
+    KC_RSFT,  KC_E,  KC_F, KC_G, \
+    KC_RCTL,  KC_LALT,  KC_SPC, KC_K \
+  ),
+/*.-----------.-----------.-----------.-----------.
+  | A         | B         |/ Play    \|/ Layer   \|
+  |           |           |\         /|\ Select  /|
+  |-----------+-----------+-----------+-----------|
+  | D         | E         | F         | G         |
+  |           |           |           |           |
+  |-----------+-----------+-----------+-----------|
+  | H         | I         | J         | K         |
+  |           |           |           |           |
+  `-----------'-----------'-----------'-----------'*/
+  [_MEDIA] = LAYOUT( /* Media Control */
+    KC_A,  KC_B,  KC_MEDIA_PLAY_PAUSE, DF(_PICK), \
+    KC_D,  KC_E,  KC_F, KC_G, \
+    KC_H,  KC_I,  KC_J, KC_K \
+  ),
+/*.-----------.-----------.-----------.-----------.
+  | A         | B         |/ C       \|/ Layer   \|
+  |           |           |\         /|\ Select  /|
+  |-----------+-----------+-----------+-----------|
+  | D         | E         | F         | G         |
+  |           |           |           |           |
+  |-----------+-----------+-----------+-----------|
+  | H         | I         | J         | K         |
+  |           |           |           |           |
+  `-----------'-----------'-----------'-----------'*/
+  [_NONE] = LAYOUT( /* Useless */
+    KC_A,  KC_B,  KC_C, DF(_PICK), \
+    KC_D,  KC_E,  KC_F, KC_G, \
+    KC_H,  KC_I,  KC_J, KC_K \
   ),
 };
 
+// void encoder_update_user(uint8_t index, bool clockwise) {
+//   if (index == 0) { /* First encoder */
+//     if (clockwise) {
+//       tap_code(KC_UP);
+//     } else {
+//       tap_code(KC_DOWN);
+//     }
+//   } else if (index == 1) {
+//     if (clockwise) {
+//       tap_code(KC_RIGHT);
+//     } else {
+//       tap_code(KC_LEFT);
+//     }
+//   }
 void encoder_update_user(uint8_t index, bool clockwise) {
-  if (index == 0) { /* First encoder */
-    if (clockwise) {
-      tap_code(KC_PGDN);
-    } else {
-      tap_code(KC_PGUP);
-    }
-  } else if (index == 2) {
-    if (clockwise) {
-      tap_code(KC_UP);
-    } else {
-      tap_code(KC_DOWN);
+    if (index == 0) {
+      switch(biton32(layer_state)) {
+      case _NONE:
+        if (clockwise) {
+          tap_code(KC_MS_U);
+        } else {
+          tap_code(KC_MS_D);
+        }
+        break;
+        case _MEDIA:
+          if (clockwise) {
+            tap_code(KC_VOLU);
+          } else {
+            tap_code(KC_VOLD);
+          }
+          break;
+      default:
+        if (clockwise) {
+          tap_code(KC_UP);
+        } else {
+          tap_code(KC_DOWN);
+        }
+        break;
+      }
+  } else if (index == 1) {
+      switch(biton32(layer_state)) {
+      case _NONE:
+        if (clockwise) {
+          tap_code(KC_MS_R);
+        } else {
+          tap_code(KC_MS_L);
+        }
+        break;
+        case _MEDIA:
+          if (clockwise) {
+            tap_code(KC_MEDIA_NEXT_TRACK);
+          } else {
+            tap_code(KC_MEDIA_PREV_TRACK);
+          }
+          break;
+      default:
+        if (clockwise) {
+          tap_code(KC_RGHT);
+        } else {
+          tap_code(KC_LEFT);
+        }
+        break;
+      }
     }
   }
-}
